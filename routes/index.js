a55b07d2c84bd41855163fb82ab0267e640aa6d5
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-26 20:46:16 +0400

    update routes/index.js logic

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 
 import authRoutes from './auth.js';
+import customerRoutes from './customers.js';
 
 
 const router = Router();
 
 router.use('/users', authRoutes);
+router.use('/customers', customerRoutes);
 
 export default router;
