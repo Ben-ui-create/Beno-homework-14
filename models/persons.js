81869dc64d9f954a37b5feecd2a34fdc71f633ac
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-26 21:44:14 +0400

    added models/persons.js logic

---
+++ models/persons.js
@@ -0,0 +1,94 @@
+import DbMysql from "../clients/db.mysql.js";
+import _ from 'lodash';
+
+export async function create(person) {
+  try {
+    const sql = `
+        INSERT INTO Persons (FirstName, LastName)
+        VALUES (?, ?);
+    `;
+
+    const values = [person.FirstName, person.LastName];
+
+    const [result = null] = await DbMysql.query(sql, values);
+
+    return {
+      PersonId: result.insertId,
+      ...person,
+    };
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export async function findAll() {
+  try {
+    const [result = null] = await DbMysql.query(
+      `            SELECT PersonID, FirstName, LastName
+                   FROM Persons
+                   ORDER BY PersonID;`);
+
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export async function update(id, data) {
+  try {
+    const sql = `
+        UPDATE Persons
+        SET FirstName = ?,
+            LastName  = ?
+        WHERE PersonID = ?
+    `;
+
+    const values = [data.FirstName, data.LastName, id];
+
+    const [result = null] = await DbMysql.query(sql, values);
+
+    return result.affectedRows > 0;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export async function deletePerson(id) {
+  try {
+    const [ordersBefore] = await DbMysql.query(
+      `
+          SELECT COUNT(*) AS count
+          FROM Orders
+          WHERE PersonID = ?
+      `, [id]);
+
+    const count = _.get(ordersBefore, '[0].count', 0);
+
+    const [result = null] = await DbMysql.query(
+      `            DELETE
+                   FROM Persons
+                   WHERE PersonID = ?;`, [id]);
+
+    if (result.affectedRows === 0) {
+      return null;
+    }
+
+    return {
+      deleted: true,
+      count: count,
+    };
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export default {
+  create,
+  findAll,
+  update,
+  deletePerson,
+}
