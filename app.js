8bed881ed0a065f4eb9eeaedc13ab0a5db38f100
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-26 18:44:32 +0400

    added app.js logic

---
+++ app.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import morgan from 'morgan';
+import path from 'path';
+import express from 'express';
+import {createServer} from 'http';
+import cookieParser from 'cookie-parser';
+
+import './migrate.js';
+
+import routes from './routes/index.js';
+import errorHandler from './middlewares/errorHandler.js';
+
+const app = express();
+
+const {PORT, COOKIE_SECRET} = process.env;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(COOKIE_SECRET));
+
+app.use(routes);
+
+app.use(errorHandler.notFound);
+app.use(errorHandler.errors);
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
